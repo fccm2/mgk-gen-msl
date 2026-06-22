eee28da03a9b6a2d79f8141ad79826dad8000d9c
Florent Monnier <monnier.florent@gmail.com>
2026-06-22 22:02:19 +0200

    get-the-generic-ttf; from IM-7

--- ext/dep.mk
+++ ext/dep.mk
@@ -1,6 +1,11 @@
 GET=wget
 all: 7.1.1-47.tar.gz
+ttf: Generic.ttf
 7.1.1-47.tar.gz:
 	$(GET) https://github.com/ImageMagick/ImageMagick/archive/refs/tags/7.1.1-47.tar.gz
+unar: 7.1.1-47.tar.gz
+	tar xzf 7.1.1-47.tar.gz
+Generic.ttf:
+	cp ImageMagick-7.1.1-47/PerlMagick/t/Generic.ttf ./
 clean:
 	$(RM) 7.1.1-47.tar.gz
